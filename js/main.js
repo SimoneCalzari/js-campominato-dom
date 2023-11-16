001a8824266b6da17e72654425f5c5a38cd05496
SimoneCalzari <simone.calzari@outlook.it>
2023-11-17 00:37:05 +0100

    fix: modifica creando tante piccole funzioni

--- js/main.js
+++ js/main.js
@@ -1,83 +1,30 @@
 'use strict';
 
-//INIZIO FUNZIONI
+//******* INIZIO FUNZIONI
 
-// FUNZIONE CREAZIONE,INSERIMENTO E AGGIUNTA EVENTLISTENER ALLE CELLE
-function newElements(typeElement, classElement, numElements) {
-  //CICLO FOR PER CREARE E INSERIRE UN CERTO NUMERO DI CELLE
-  for (let i = 1; i <= numElements; i++) {
-    const cell = document.createElement(typeElement); // CREO NUOVA CELLA
-    cell.classList = classElement; // ASSEGNO LA CLASSE
-    cell.innerHTML = `${i}`; // INSERISCO CONTENUTO
-    field.append(cell); // INSERISCO NELL HTML DENTRO AL FIELD
-    field.classList.remove('no-click'); // TOLGO AL FIELD LA CLASSE CHE IMPEDISCE DI CLICCARE CHE VIENE INSERITA A FINE PARTITA
-    // EVENT LISTENER ALLE CELLE
-    cell.addEventListener('click', function() {
-        if (positionBombs.includes(Number(cell.innerHTML))) {
-          cell.classList.add('cell-bomb'); // AGGIUNGO CLASSE PER CASO CELLA CON BOMBA
-          field.classList.add('no-click'); // IMPEDISCO DI CLICCARE A FINE PARTITA
-          showResult.innerHTML = `Hai perso! Grazie per aver giocato, il tuo punteggio è stato di ${counterNoBomb}`;
-          // QUANDO CLICCO SU UNA BOMBA SCOPRO ANCHE TUTTE LE ALTRE
-          for (let j = 1; j <= numElements; j++) {
-            const div = document.querySelector(`.field div:nth-child(${j})`);
-            if (positionBombs.includes(Number(div.innerHTML))) {
-              div.classList.add('cell-bomb');
-            }
-          }
-        } 
-        else {
-          cell.classList.add('cell-no-bomb'); // AGGIUNGO CLASSE PER CASO CELLA SENZA BOMBA
-          cell.classList.add('no-click'); // IMPEDISCO DI CLICCARE SU UNA CELLA GIA CLICCATA
-          counterNoBomb++; // INCREMENTO PUNTEGGIO AD OGNI CELLA NON BOMBA CLICCATA
-          showResult.innerHTML = `Continua così! Il tuo punteggio al momento è di ${counterNoBomb}`;
-          // CASO TUTTE LE CASELLE SENZA BOMBA TROVATE
-          if (counterNoBomb === numElements - numBombs) {
-            showResult.innerHTML = `Ha vinto! Il tuo punteggio è stato di ${counterNoBomb}`;
-          }
-        }
-      } 
-    )
+// GENERA CELLA CON CLASSE CSS
+function newCell(typeElement,classElement) {
+  const cell = document.createElement(typeElement); 
+  cell.classList = classElement; 
+  return cell;
+}
+
+// GENERA LA GRIGLIA E SALVA ELEMENTI IN UN ARRAY
+function createGrid(typeCell, classCell, numCells) {
+  arrayCells = [];
+  for (let i = 1; i <= numCells; i++) {
+    const cellInGrid = newCell(typeCell, classCell);
+    cellInGrid.innerHTML = `${i}`;
+    field.append(cellInGrid);
+    arrayCells.push(cellInGrid);
   }
 }
-
-
-// FUNZIONE CHE AGGIUNGE UN EVENTLISTENER AL BOTTONE PLAY QUANDO CLICCO PER GENERARE IL GIOCO
-function myGame() {
-  button.addEventListener('click', 
-    function() {
-      counterNoBomb = 0; // AZZERO PUNTEGGIO ALL'INIZIO DI OGNI PARTITA
-      showResult.innerHTML = 'Seleziona la difficoltà e poi clicca su Play per cominciare a giocare !';
-      field.innerHTML = ''; // SVUOTO FIELD ALTRIMENTI ANDREI AD AGGIUNGERE CELLE AD UN SECONDO CLICK
-      field.classList.add('active'); // FIELD DI DEFAULT NON VISIBILE, AGGIUNGO CLASSE PER RENDERLO VISIBILE
-      // CASO DIFFICOLTà DIFFICILE
-      if (select.value === 'Hard'){
-        newElements('div', 'cell-7', 49);
-        createBombs(numBombs, 49);
-        console.log(positionBombs);
-      } 
-      // CASO DIFFICOLTà MEDIA
-      else if (select.value === 'Medium'){
-        newElements('div', 'cell-9', 81);
-        createBombs(numBombs, 81);
-        console.log(positionBombs);
-      }
-      // CASO DIFFICOLTà FACILE
-      else {
-        newElements('div', 'cell-10', 100);
-        createBombs(numBombs, 100);
-        console.log(positionBombs);
-      }
-    }
-  )
-}
-
 
 // FUNZIONE CALCOLO NUMERO RANDOM TRA DUE NUMERI, ESTREMI COMPRESI
 function randomTraNumeriCompresi(inf,sup) {
   const numRandom = inf + Math.floor(Math.random() * (sup - inf + 1));
   return numRandom;
 }
-
 
 // FUNZIONE PER GENERARE ARRAY CON POSIZIONE BOMBE
 function createBombs(numBombs, maxCells) {
@@ -90,30 +37,78 @@ function createBombs(numBombs, maxCells) {
   }
 }
 
-//FINE FUNZIONI
+// FUNZIONE CHE MOSTRA ROSSE TUTTE LE CELLE CON LE BOMBE
+function bombCellRed(numCells) {
+  for (let i = 0; i < numCells; i++) {
+    if (positionBombs.includes(Number(arrayCells[i].innerHTML))) {
+      arrayCells[i].classList.add('cell-bomb');
+    }
+  }
+}
+
+// FUNZIONE CHE CREA IL COMPORTAMENTO AL CLICK DELLE CELLE
+function clickOnCell(numCells) {
+  for (let i = 0; i < numCells; i++) {
+    arrayCells[i].addEventListener('click', function() {
+      if (positionBombs.includes(Number(arrayCells[i].innerHTML))) {
+        arrayCells[i].classList.add('cell-bomb');
+        field.classList.add('no-click');
+        showResult.innerHTML = `Hai perso! Grazie per aver giocato, il tuo punteggio è stato di ${counterNoBomb}`;
+        bombCellRed(numCells);
+      } 
+      else {
+        arrayCells[i].classList.add('cell-no-bomb','no-click');
+        counterNoBomb++;
+        showResult.innerHTML = `Continua così! Il tuo punteggio al momento è di ${counterNoBomb}`;
+        if (counterNoBomb === numCells - numBombs) {
+          showResult.innerHTML = `Ha vinto! Il tuo punteggio è stato di ${counterNoBomb}`;
+          field.classList.add('no-click'); 
+        }
+      }
+    });
+  }
+}
+
+// FUNZIONE PRINCIPALE CHE CREA LA PARTITA
+function myGame() {
+  button.addEventListener('click', function() {
+    field.innerHTML = ''; 
+    field.classList.add('active');
+    field.classList.remove('no-click');
+    showResult.innerHTML = 'Seleziona la difficoltà e poi clicca su Play per cominciare a giocare !';
+    counterNoBomb = 0; 
+    if (select.value === 'Hard') {
+      createGrid('div', 'cell-7', 49);
+      createBombs(numBombs, 49);
+      clickOnCell(49);
+    } 
+    else if (select.value === 'Medium') {
+      createGrid('div', 'cell-9', 81);
+      createBombs(numBombs, 81);
+      clickOnCell(81);
+    }
+    else {
+      createGrid('div', 'cell-10', 100);
+      createBombs(numBombs, 100);
+      clickOnCell(100);
+    }
+  });
+}
+
+//******* FINE FUNZIONI
 
 
+//******* INIZIO LOGICA PROGRAMMA
 
-
-// LOGICA PROGRAMMA
-
-// VARIABILI DEGLI ELEMENTI DEL DOM UTILIZZATI NELLA FUNCTION
 const field = document.querySelector('div.field');
 const button = document.querySelector('button');
 const select = document.querySelector('select');
 const showResult = document.getElementById('vinto-perso')
-
-
-const numBombs = 16; // NUMERO DI BOMBE DA INSERIRE
-let positionBombs = []; // POSIZIONI BOMBE
-let counterNoBomb = 0; // PUNTEGGIO
-
-
-// CHIAMO LA FUNZIONI PER IL MIO GIOCO
+const numBombs = 16; 
+let positionBombs = [];
+let arrayCells = []; 
+let counterNoBomb = 0; 
 myGame();
-
-
-//TODO: CODICE DA RIVEDERE PER EVITARE LA PRIMA MACRO FUNZIONE CHE FA TROPPE COSE!!!!
 
 
 
